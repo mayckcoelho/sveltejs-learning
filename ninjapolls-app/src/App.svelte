48dbb484a22cec13a828dffe4c5f8d2de30f5053
Mayck Coelho <mayck.coelho@riachuelo.com.br>
2020-04-16 16:38:12 -0300

    Commit inicial do projeto de polls

---
+++ ninjapolls-app/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Header from "./components/Header.svelte";
+  import Footer from "./components/Footer.svelte";
+</script>
+
+<style>
+  main {
+    max-width: 960px;
+    margin: 40px auto;
+  }
+</style>
+
+<Header />
+<main />
+<Footer />
